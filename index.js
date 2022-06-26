a3ce5a2fec21387e87c8116a8530e16202e2fdd9
unknown <ghaith990000@gmail.com>
2022-06-26 16:03:18 +0300

    final version of accordion using only html and css

---
+++ index.js
@@ -0,0 +1,11 @@
+const accordion = document.getElementsByClassName('contentBx');
+console.log(accordion);
+
+function showAccordionItem(){
+    for(let i=0; i<accordion.length; i++){
+        accordion[i].addEventListener('click', function(accordionItem){
+            accordionItem
+        });
+    }
+}
+
